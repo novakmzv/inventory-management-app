fb22eda2575d67735d1a7743d086f723bfe90719
novak <novakmzv@gmail.com>
2025-08-02 14:38:55 -0500

    feat: create index for global components

---
+++ src/core/components/index.ts
@@ -0,0 +1,5 @@
+export * from './confirmation-modal/confirmation-modal.component';
+
+export * from './notification/notification.component';
+
+export * from './loading-overlay/loading-overlay.component';
