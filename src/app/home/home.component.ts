8d24a60dedf01aaaa70a47227d348449181258f3
novak <novakmzv@gmail.com>
2025-08-02 16:44:21 -0500

    refactor: modify HomeComponent

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,25 +1,46 @@
 import {Component, inject} from '@angular/core';
-import {Router} from '@angular/router';
-import {Package, ShoppingCart, Edit, ClipboardList, LucideAngularModule} from 'lucide-angular';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { LucideAngularModule, Package, ShoppingCart, Edit, ClipboardList, Archive, Users } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    LucideAngularModule
-  ],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   readonly Package = Package;
+  readonly Archive = Archive;
+  readonly Users = Users;
   readonly ShoppingCart = ShoppingCart;
   readonly Edit = Edit;
   readonly ClipboardList = ClipboardList;
 
-  private router: Router = inject(Router)
+  private router: Router = inject(Router);
 
   async navigateToProducts() {
     await this.router.navigate(['/products']);
+  }
+
+  async navigateToBatches() {
+    await this.router.navigate(['/batches']);
+  }
+
+  navigateToClients() {
+    console.log('Navegando a clientes...');
+  }
+
+  navigateToShopping() {
+    console.log('Navegando a compras...');
+  }
+
+  navigateToNotes() {
+    console.log('Navegando a notas...');
+  }
+
+  navigateToPlan() {
+    console.log('Navegando a plan...');
   }
 }
