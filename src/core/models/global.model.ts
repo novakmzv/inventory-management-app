6f009120b184edd46f834009bdf7292c8a91fa78
novak <novakmzv@gmail.com>
2025-08-02 16:57:57 -0500

    feat: added new global models

--- src/core/models/global.model.ts
+++ src/core/models/global.model.ts
@@ -4,4 +4,18 @@ export interface ApiResponse<T> {
   message: string;
 }
 
+export interface PaginatedResponse<T> {
+  data: T[];
+  current_page: number;
+  last_page: number;
+  per_page: number;
+  total: number;
+}
+
+export interface SelectOption<T> {
+  value: T;
+  label: string;
+  disabled?: boolean;
+}
+
 export type NotificationType = 'success' | 'error' | 'warning';
