1b389e42c9e9f1fbf81fd6c1e5c2e079c4015ff2
novak <novakmzv@gmail.com>
2025-08-02 16:57:42 -0500

    refactor: modify routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
   {
     path: 'products',
     loadChildren: () => import('./products/products.routes').then(m => m.productsRoutes)
+  },
+  {
+    path: 'batches',
+    loadChildren: () => import('./batches/batches.routes').then(m => m.batchesRoutes)
   },
 ];
--- src/app/products/products.routes.ts
+++ src/app/products/products.routes.ts
@@ -1,7 +1,6 @@
 import {Routes} from '@angular/router';
 import {ListProductsComponent, CreateProductComponent} from '.';
 
-
 export const productsRoutes: Routes = [
   {path: '', redirectTo: 'list', pathMatch: 'full'},
   {path: 'list', component: ListProductsComponent},
